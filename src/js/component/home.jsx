95ecccf05501513cd9b4dcce82ee13743993d61e
omar saadane el meski <o_omar_96@hotmail.com>
2024-01-27 16:32:19 +0000

    semaforo finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,11 +4,11 @@ const Semaforo = () => {
 	const [color, setColor] = useState('')
     setInterval (()=>{
 		let colors = ['red','yellow','green']
-		let randomColorsIndex = Math.floor(Math.random() * 3)
+		let randomColorsIndex = Math.floor(Math.random()* 3)
 		setColor(colors[randomColorsIndex]);
-	}, 2000);
+	}, 3000);
 	return (
-		<div className='container mt-5 bg-dark p-4'>
+		<div className='container text-center p-3 bg-black' style={{width: "100px", height:"230px"}}>
 			<div className={`rounded-circle bg-danger mx-auto ${color === 'red'? 'sombra': ''}`} style={{width: '60px', height:'60px'}} onClick={()=>{ setColor('red')}}></div>
 			<div className={`rounded-circle bg-warning mx-auto ${color === 'yellow'? 'sombra': ''}`}style={{width: '60px', height:'60px'}} onClick={()=>{ setColor('yellow')}}></div>
 			<div className={`rounded-circle bg-success mx-auto ${color === 'green'? 'sombra': ''}`} style={{width: '60px', height:'60px'}} onClick={()=>{ setColor('green')}}></div>
